55edcd2bf8cb1b623ca733a5147fe9f0c3ac0c62
HustMaroon <thanhbk6294@gmail.com>
2020-05-26 16:51:20 +0900

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_075748) do
+ActiveRecord::Schema.define(version: 2020_05_26_074649) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
